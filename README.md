fc4b09cc89e27074987778721a30d151fb9919e7
patrick okafor chibuike <patrickpeters911@gmail.com>
2022-11-02 18:07:46 +0100

    make search filter

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Search-Bar-with-Filter
+
+<h2>A Javascript project to search and filter words to display according to the input value.</h2>
+
+<h4>Tools used</h4>
+<p>JavaScript</p>
+<p>Bootsrap</p>
+<p>HTML5</p>
+<p>CSS3</p>
---
+++ index.js
@@ -0,0 +1,24 @@
+search = () => {
+let input = document.getElementById('myInput');
+let filter = input.value.toUpperCase();
+let ul = document.getElementById('myUL')
+let li = ul.getElementsByTagName('li')
+let span = document.querySelector('SPAN')
+
+
+for (let i = 0; i < li.length; i++) {
+ let a = li[i].getElementsByTagName('a')[0];
+ 
+ if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+    li[i].style.display = '';
+
+    
+ }else{
+    li[i].style.display ='none';
+    span.innerHTML = 'Sorry, never heard this'
+
+
+ }
+}
+}
